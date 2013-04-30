f38135955e906ca0b802b288e116250436611fb5
PommesDieFritte <pommesdiefritte@gmx.de>
2013-04-30 18:24:52 +0200

    Added basic Button class.

---
+++ include/gui/button.hpp
@@ -0,0 +1,51 @@
+#ifndef QRW_BUTTON_HPP
+#define QRW_BUTTON_HPP
+
+#include <string>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Window/Event.hpp>
+
+namespace qrw
+{
+	class Button : public sf::Sprite
+	{
+		public:
+			Button(sf::Window* window = NULL, std::string text = "",
+				const sf::Texture* textureactive = NULL,
+				const sf::Texture* textureinainactive = NULL,
+				const sf::Texture* texutrehover = NULL);
+			~Button();
+
+			void setText(std::string text);
+			std::string getText();
+
+			void setTextures(const sf::Texture* textureinactive,
+				const sf::Texture* textureactive, 
+				const sf::Texture* texturehover);
+
+			// void draw(sf::RenderTarget& target,
+			// 	sf::RenderStates states = sf::RenderStates::Default) const;
+			void handleEvent(const sf::Event& event);
+
+		private:
+			bool mouseOnButton();
+			void updateSprite();
+
+			enum STATES
+			{
+				ES_INACTIVE,
+				ES_ACTIVE,
+				ES_HOVER
+			};
+
+			std::string text;
+			STATES state;
+			sf::Window* window;
+
+			const sf::Texture* textures[3];
+	};
+}
+
+#endif
---
+++ src/gui/button.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+#include <SFML/Window/Mouse.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+
+#include "gui/button.hpp"
+
+namespace qrw
+{
+
+	Button::Button(sf::Window* window,std::string text,
+		const sf::Texture* textureactive,
+		const sf::Texture* textureinactive,
+		const sf::Texture* texturehover)
+	: Sprite(),
+	  text(text),
+	  window(window),
+	  state(ES_INACTIVE)
+	{
+		if(textureactive != NULL && textureinactive != NULL
+			&& texturehover != NULL)
+			setTextures(textureactive, textureinactive, texturehover);
+
+		for(int i = 0; i < 3; ++i)
+			textures[i] = 0;
+	}
+
+	Button::~Button()
+	{}
+
+	void Button::setText(std::string text)
+	{
+		this->text = text;
+	}
+	std::string Button::getText()
+	{
+		return text;
+	}
+
+	void Button::setTextures(const sf::Texture* textureinactive,
+		const sf::Texture* textureactive, const sf::Texture* texturehover)
+	{
+		textures[ES_INACTIVE] = textureinactive;
+		textures[ES_ACTIVE] = textureactive;
+		textures[ES_HOVER] = texturehover;
+	}
+
+	// void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const
+	// {
+	// 	target.draw(*(sf::Sprite*)this, states);
+	// }
+
+	void Button::handleEvent(const sf::Event& event)
+	{
+		if(mouseOnButton() == false)
+		{
+			state = ES_INACTIVE;
+			updateSprite();
+			return;
+		}
+		if(event.type == sf::Event::MouseMoved)
+			state = ES_HOVER;
+		else if(event.type == sf::Event::MouseButtonPressed &&
+			event.mouseButton.button == sf::Mouse::Left)
+		{
+			state = ES_ACTIVE;
+		}
+		else if(event.type == sf::Event::MouseButtonReleased &&
+			event.mouseButton.button == sf::Mouse::Left)
+		{
+			state = ES_INACTIVE;
+		}
+		updateSprite();
+	}
+
+	bool Button::mouseOnButton()
+	{
+		sf::FloatRect bounds = getGlobalBounds();
+		sf::Vector2f mousepos;
+		mousepos.x = (float)sf::Mouse::getPosition(*window).x;
+		mousepos.y = (float)sf::Mouse::getPosition(*window).y;
+		if(mousepos.x < bounds.left || mousepos.x > (bounds.left + bounds.width)
+			|| mousepos.y < bounds.top || mousepos.y > (bounds.top + bounds.height))
+		{
+			return false;
+		}
+		return true;
+	}
+
+	void Button::updateSprite()
+	{
+		if(textures[state] != 0)
+			setTexture(*textures[state]);
+	}
+}
