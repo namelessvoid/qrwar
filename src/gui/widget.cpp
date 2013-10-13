76b8bfe411c3f5e76204f5909afb7628a4e23e97
PommesDieFritte <pommesdiefritte@gmx.de>
2013-10-13 19:21:43 +0200

    Implemented Widget::handleEvent (with debug sysouts).

--- src/gui/widget.cpp
+++ src/gui/widget.cpp
@@ -2,6 +2,8 @@
 
 #include <SFML/Window/Mouse.hpp>
 
+#include <iostream>
+
 namespace qrw
 {
     Widget::Widget(sf::Window* window)
@@ -27,6 +29,44 @@ namespace qrw
 
     void Widget::handleEvent(const sf::Event& event)
     {
+        // Handle mouse move evets
+        if(event.type == sf::Event::MouseMoved)
+        {
+            if(!hasMouseFocus())
+            {
+                // Mouse focus lost
+                if(mouseFocus)
+                {
+                    mouseFocus = false;
+                    signalmouseleft.emit();
+                    std::cout << "Mouse left widget.\n";
+                }
+                return;
+            } // if(!hasMouseFocus)
+            else
+            {
+                // Got mouse focus
+                if(!mouseFocus)
+                {
+                    mouseFocus = true;
+                    signalmouseentered.emit();
+                    std::cout << "mouse entered widget.\n";
+                }
+                return;
+            } // else(hasMouseFocus)
+        } // if(MouseMoveEvent)
+        else if(event.type == sf::Event::MouseButtonReleased)
+        {
+            if(mouseFocus)
+            {
+                if(event.mouseButton.button == sf::Mouse::Left)
+                {
+                    signalclicked.emit();
+                    std::cout << "Widget was clicked.\n";
+                    return;
+                }
+            }
+        } // else(MouseButtonEvent)
 
     }
 }
