0a608c67de3620428ac083c8570ac913b2df9329
PommesDieFritte <pommesdiefritte@gmx.de>
2013-10-13 16:54:11 +0200

    Added basic Widget functinality (yet untested.)

--- include/gui/widget.hpp
+++ include/gui/widget.hpp
@@ -1,16 +1,30 @@
 #ifndef WIDGET_HPP
 #define WIDGET_HPP
 
+#include <SFML/Window/Window.hpp>
 #include <SFML/Graphics/Sprite.hpp>
+
+#include "gui/signal.hpp"
 
 namespace qrw
 {
-    class Widget : sf::Sprite
+    class Widget : public sf::Sprite
     {
         public:
-            Widget();
+            Widget(sf::Window* window);
             ~Widget();
-        private:
+
+            void handleEvent(sf::Event& event);
+
+            // Signals
+            Signal signalclicked;
+            Signal signalmouseentered;
+            Signal signalmouseleft;
+
+        protected:
+            bool hasMouseFocus();
+
+            sf::Window* window;
     };
 }
 #endif
--- src/gui/widget.cpp
+++ src/gui/widget.cpp
@@ -1,12 +1,31 @@
 #include "gui/widget.hpp"
 
+#include <SFML/Window/Mouse.hpp>
+
 namespace qrw
 {
-    Widget::Widget()
+    Widget::Widget(sf::Window* window)
     {
+        this->window = window;
     }
 
     Widget::~Widget()
     {
+    }
+
+    bool Widget::hasMouseFocus()
+	{
+		sf::FloatRect bounds = getGlobalBounds();
+
+		sf::Vector2f mousepos;
+		mousepos.x = (float)sf::Mouse::getPosition(*window).x;
+		mousepos.y = (float)sf::Mouse::getPosition(*window).y;
+
+		return bounds.contains(mousepos);
+	}
+
+    void Widget::handleEvent(sf::Event& event)
+    {
+
     }
 }
