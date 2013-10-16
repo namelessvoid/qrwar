b43123b4f333be03824c3bc9a14ae028447cbf1d
namelessvoid <pommesdiefritte@gmx.de>
2013-10-16 21:51:00 +0200

    Merged src/gui/widget.cpp which i forgot to merge in commit 6113617. (stupid me *slap*)

--- src/gui/widget.cpp
+++ src/gui/widget.cpp
@@ -101,14 +101,11 @@ namespace qrw
         } // else(MouseButtonEvent)
     }
 
-<<<<<<< HEAD
-=======
     void Widget::disconnectAllSignals()
     {
         this->signalclicked.disconnectAll();
         this->signalleftmousebuttonpressed.disconnectAll();
         this->signalmouseleft.disconnectAll();
         this->signalmouseentered.disconnectAll();
->>>>>>> origin/signal-refactoring
     }
 }
