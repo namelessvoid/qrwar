16c381fabfe89b4ec41721895a8d5f4a6cdf1d2c
namelessvoid <pommesdiefritte@gmx.de>
2013-12-07 21:21:25 +0100

    Added some basic attributes to VideoSettings.

--- include/config/videosettings.hpp
+++ include/config/videosettings.hpp
@@ -1,11 +1,22 @@
 #ifndef QRW_VIDEOSETTINGS_HPP
 #define QRW_VIDEOSETTINGS_HPP
 
+#include <string>
+
 namespace qrw
 {
 	class VideoSettings
 	{
+		public:
+			VideoSettings();
+			~VideoSettings();
 
+			bool loadFromFile(std::string path);
+
+		protected:
+			int resolutionX;
+			int resolutionY;
+			bool fullscreen;
 	};
 }
 #endif
--- src/config/videosettings.cpp
+++ src/config/videosettings.cpp
@@ -1,1 +1,18 @@
 #include "config/videosettings.hpp"
+
+namespace qrw
+{
+	VideoSettings::VideoSettings()
+		:	resolutionX(800),
+			resolutionY(600),
+			fullscreen(false)
+	{}
+
+	VideoSettings::~VideoSettings()
+	{}
+
+	bool VideoSettings::loadFromFile(std::string filepath)
+	{
+		return false;
+	}
+}
