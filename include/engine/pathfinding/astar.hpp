ec2234b6b49a30a69c26a0e92cb6b83f842c0802
namelessvoid <pommesdiefritte@gmx.de>
2013-12-28 17:05:50 +0100

    Added empty classes for the A* algorithm.

---
+++ include/engine/pathfinding/astar.hpp
@@ -0,0 +1,13 @@
+#ifndef QRW_ASTAR_HPP
+#define QRW_ASTAR_HPP
+
+#include "engine/pathfinding/abstractalgorithm.hpp"
+
+namespace qrw
+{
+	class AStar : public AbstractAlgorithm
+	{
+
+	};
+}
+#endif
---
+++ test/engine/pathfinding/astartest.cpp
@@ -0,0 +1,13 @@
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestFixture.h>
+
+#include "engine/pathfinding/astar.hpp"
+
+class AStarTest : public CppUnit::TestFixture
+{
+	CPPUNIT_TEST_SUITE(AStarTest);
+	CPPUNIT_TEST_SUITE_END();
+
+	public:
+};
+CPPUNIT_TEST_SUITE_REGISTRATION(AStarTest);
