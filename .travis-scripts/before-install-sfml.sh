29c744b6f4382b6ab8c11ac42807e82263732617
namelessvoid <pommesdiefritte@gmx.de>
2014-01-05 17:43:29 +0100

    Set up build for SFML bleeding edge version.

--- .travis-scripts/before-install-sfml.sh
+++ .travis-scripts/before-install-sfml.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env sh
 
-echo cloning SFML...
+sudo apt-get install libpthread-stubs0-dev
+sudo apt-get install libgl1-mesa-dev
+sudo apt-get install libx11-dev
+sudo apt-get install libxrandr-dev
+sudo apt-get install libfreetype6-dev
+sudo apt-get install libglew1.5-dev
+sudo apt-get install libjpeg8-dev
+sudo apt-get install libsndfile1-dev
+sudo apt-get install libopenal-dev
+
+cd ..
 git clone https://github.com/LaurentGomila/SFML.git
-echo DONE
+cd SFML
+cmake . && make && sudo make install
