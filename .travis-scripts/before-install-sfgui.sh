96978e0778df8a85308357aa586248218d13ad8d
namelessvoid <pommesdiefritte@gmx.de>
2014-01-07 21:56:47 +0100

    Fixed copying of FindSFGUI.cmake.

--- .travis-scripts/before-install-sfgui.sh
+++ .travis-scripts/before-install-sfgui.sh
@@ -9,6 +9,6 @@ cd SFGUI
 cmake . && make && sudo make install
 
 #copy FindSFGUI.cmake
-sudo cp cmake/Modules/FindSFGUI.cmake /usr/share/cmake-2.8/Modules
+sudo cp cmake/FindSFGUI.cmake /usr/share/cmake-2.8/Modules
 
 cd $TRAVIS_BUILD_DIR
