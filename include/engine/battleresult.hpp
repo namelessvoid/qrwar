db89f835c50146cecf2553f9c20e52de6c4cb012
namelessvoid <pommesdiefritte@gmx.de>
2014-08-07 20:48:54 +0200

    Added struct BattleResult.

---
+++ include/engine/battleresult.hpp
@@ -0,0 +1,15 @@
+#ifndef QRW_BATTLERESULT_HPP
+#define QRW_BATTLERESULT_HPP
+
+namespace qrw
+{
+
+struct BattleResult
+{
+	int attackerHPDelta;
+	int defenderHPDelta;
+};
+
+} // namespace qrw
+
+#endif // QRW_BATTLERESULT_HPP
