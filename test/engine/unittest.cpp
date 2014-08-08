6d02fbc0384e0da8b090b4f251e1bb870e13d48b
namelessvoid <pommesdiefritte@gmx.de>
2014-08-08 16:42:45 +0200

    Simplified Unit unittests.

--- test/engine/unittest.cpp
+++ test/engine/unittest.cpp
@@ -32,14 +32,27 @@ class UnitTest : public CppUnit::TestFixture
 	public:
 		void setUp()
 		{
-			unit1 = new qrw::Unit(qrw::EUT_SWORDMAN, 5, 2, 1, 0, 0, 0, nullptr);
-			unit2 = new qrw::Unit(qrw::EUT_SWORDMAN, 5, 3, 1, 0, 0, 0, nullptr);
+			board = new qrw::Board(3, 3);
+			player1 = new qrw::Player();
+			player2 = new qrw::Player();
+			unit1 = new qrw::Unit(qrw::EUT_SWORDMAN, 5, 2, 1, 1, 4, player1, board);
+			unit2 = new qrw::Unit(qrw::EUT_SWORDMAN, 5, 3, 1, 1, 4, player2, board);
+
+			// Default position of units
+			unit1->setSquare(board->getSquare(0, 0));
+			unit1->getSquare()->setUnit(unit1);
+
+			unit2->setSquare(board->getSquare(0, 1));
+			unit2->getSquare()->setUnit(unit2);
 		}
 
 		void tearDown()
 		{
 			delete unit1;
 			delete unit2;
+			delete player1;
+			delete player2;
+			delete board;
 		}
 
 		void setHPTest()
@@ -62,8 +75,7 @@ class UnitTest : public CppUnit::TestFixture
 
 		void removeFromBoardTest()
 		{
-			qrw::Board board(3, 3);
-			qrw::Square* square = board.getSquare(0, 0);
+			qrw::Square* square = board->getSquare(0, 0);
 			unit1->setSquare(square);
 			square->setUnit(unit1);
 
@@ -104,192 +116,131 @@ class UnitTest : public CppUnit::TestFixture
 
 		void canMoveToTestPlayerNotActive()
 		{
-			qrw::Board board(3, 3);
-			qrw::Player player;
-			player.setActive(false);
-
-			qrw::Unit unit(qrw::EUT_SPEARMAN, 5, 2, 1, 1, 3, &player, &board);
-
-			CPPUNIT_ASSERT(unit.canMoveTo(qrw::Coordinates(1, 1)) == nullptr);
+			player1->setActive(false);
+			CPPUNIT_ASSERT(unit1->canMoveTo(qrw::Coordinates(1, 1)) == nullptr);
 		}
 
 		void canMoveToTestDestinationNotEmpty()
 		{
-			qrw::Board board(3, 3);
-			qrw::Player player;
-			player.setActive(true);
+			player1->setActive(true);
+			board->getSquare(0, 0)->setUnit(unit1);
+			board->getSquare(2, 0)->setUnit(unit2);
 
-			qrw::Unit blocker(qrw::EUT_SPEARMAN, 5, 2, 1, 1, 3, nullptr, &board);
-			board.getSquare(2, 0)->setUnit(&blocker);
-
-			qrw::Unit mover(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player, &board);
-			board.getSquare(0, 0)->setUnit(&mover);
-
-			CPPUNIT_ASSERT(mover.canMoveTo(qrw::Coordinates(2, 0)) == nullptr);
+			CPPUNIT_ASSERT(unit1->canMoveTo(qrw::Coordinates(2, 0)) == nullptr);
 		}
 
 		void canMoveToTestNotEnoughMovement()
 		{
-			qrw::Board board(3, 3);
-			qrw::Player player;
-			player.setActive(true);
+			player1->setActive(true);
 
-			qrw::Unit unit(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player, &board);
-			unit.setSquare(board.getSquare(0, 0));
-			board.getSquare(0, 0)->setUnit(&unit);
+			unit1->setSquare(board->getSquare(0, 0));
+			unit1->getSquare()->setUnit(unit1);
 
-			unit.setCurrentMovement(1);
+			unit1->setCurrentMovement(1);
 
-			CPPUNIT_ASSERT(unit.canMoveTo(qrw::Coordinates(2, 0)) == nullptr);
+			CPPUNIT_ASSERT(unit1->canMoveTo(qrw::Coordinates(2, 0)) == nullptr);
 		}
 
 		void canMoveToTest()
 		{
-			qrw::Board board(3, 3);
-			qrw::Player player;
-			player.setActive(true);
+			player1->setActive(true);
 
-			qrw::Unit unit(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player, &board);
-			unit.setSquare(board.getSquare(0, 0));
-			board.getSquare(0, 0)->setUnit(&unit);
+			unit1->setSquare(board->getSquare(0, 0));
+			unit1->getSquare()->setUnit(unit1);
+			unit1->setCurrentMovement(3);
 
-			unit.setCurrentMovement(3);
-			unit.canMoveTo(qrw::Coordinates(2, 0));
-
-			CPPUNIT_ASSERT(unit.canMoveTo(qrw::Coordinates(2, 0)) != nullptr);
+			unit1->canMoveTo(qrw::Coordinates(2, 0));
+			CPPUNIT_ASSERT(unit1->canMoveTo(qrw::Coordinates(2, 0)) != nullptr);
 		}
 
 		void moveToTest()
 		{
-			qrw::Board board(3, 3);
-			qrw::Player player;
-			player.setActive(true);
+			player1->setActive(true);
 
-			qrw::Unit unit(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player, &board);
-			unit.setSquare(board.getSquare(0, 0));
-			board.getSquare(0, 0)->setUnit(&unit);
+			unit1->setSquare(board->getSquare(0, 0));
+			unit1->getSquare()->setUnit(unit1);
+			unit1->setCurrentMovement(3);
 
-			unit.setCurrentMovement(3);
-
-			unit.moveTo(qrw::Coordinates(2, 0));
+			unit1->moveTo(qrw::Coordinates(2, 0));
 
 			// Check if squares are updated
-			CPPUNIT_ASSERT(board.getSquare(0, 0)->getUnit() == nullptr);
-			CPPUNIT_ASSERT(board.getSquare(2, 0)->getUnit() == &unit);
+			CPPUNIT_ASSERT(board->getSquare(0, 0)->getUnit() == nullptr);
+			CPPUNIT_ASSERT(board->getSquare(2, 0)->getUnit() == unit1);
 			// Check if unit has updated square
-			CPPUNIT_ASSERT(unit.getSquare() == board.getSquare(2, 0));
+			CPPUNIT_ASSERT(unit1->getSquare() == board->getSquare(2, 0));
 			// Check if movement was substracted correctly
-			CPPUNIT_ASSERT(unit.getCurrentMovement() == 1);
+			CPPUNIT_ASSERT(unit1->getCurrentMovement() == 1);
 		}
 
 		void canAttackTest()
 		{
-			qrw::Board board(3, 3);
+			player1->setActive(true);
 
-			qrw::Player player1;
-			player1.setActive(true);
+			unit1->setSquare(board->getSquare(0, 0));
+			unit1->getSquare()->setUnit(unit1);
 
-			qrw::Player player2;
-			player2.setActive(false);
+			unit2->setSquare(board->getSquare(0, 1));
+			unit2->getSquare()->setUnit(unit2);
 
-			qrw::Unit attacker(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player1, &board);
-			qrw::Unit defender(qrw::EUT_SPEARMAN, 5, 2, 2, 1, 3, &player2, &board);
-
-			board.getSquare(0, 0)->setUnit(&attacker);
-			attacker.setSquare(board.getSquare(0, 0));
-
-			board.getSquare(0, 1)->setUnit(&defender);
-			defender.setSquare(board.getSquare(0, 1));
-
-			CPPUNIT_ASSERT(attacker.canAttack(&defender));
+			CPPUNIT_ASSERT(unit1->canAttack(unit2));
 		}
 
 		void canAttackTestPlayerNotActive()
 		{
-			qrw::Board board(3, 3);
-			qrw::Player player;
-			player.setActive(false);
+			player1->setActive(false);
 
-			qrw::Unit attacker(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player, &board);
-			qrw::Unit defender(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player, &board);
-
-			CPPUNIT_ASSERT_EQUAL(false, attacker.canAttack(&defender));
+			CPPUNIT_ASSERT_EQUAL(false, unit1->canAttack(unit2));
 		}
 
 		void canAttackTestUnitOfSamePlayer()
 		{
-			qrw::Board board(3, 3);
-			qrw::Player player;
-			player.setActive(true);
+			player1->setActive(true);
+			unit2->setPlayer(player1);
 
-			qrw::Unit attacker(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player, &board);
-			qrw::Unit defender(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player, &board);
-
-			board.getSquare(0, 0)->setUnit(&attacker);
-			attacker.setSquare(board.getSquare(0, 0));
-
-			board.getSquare(0, 1)->setUnit(&defender);
-			defender.setSquare(board.getSquare(0, 1));
-
-			CPPUNIT_ASSERT_EQUAL(false, attacker.canAttack(&defender));
+			CPPUNIT_ASSERT_EQUAL(false, unit1->canAttack(unit2));
 		}
 
 		void canAttackTestOutOfAttackRange()
 		{
-			qrw::Board board(3, 3);
-			qrw::Player player1;
-			qrw::Player player2;
-			player1.setActive(true);
+			player1->setActive(true);
 
-			qrw::Unit attacker(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player1, &board);
-			qrw::Unit defender(qrw::EUT_ARCHER, 5, 2, 1, 1, 3, &player2, &board);
+			unit2->removeFromBoard();
+			unit2->setSquare(board->getSquare(2, 2));
+			unit2->getSquare()->setUnit(unit2);
 
-			board.getSquare(0, 0)->setUnit(&attacker);
-			attacker.setSquare(board.getSquare(0, 0));
-
-			board.getSquare(0, 2)->setUnit(&defender);
-			defender.setSquare(board.getSquare(0, 2));
-
-			CPPUNIT_ASSERT_EQUAL(false, attacker.canAttack(&defender));
+			CPPUNIT_ASSERT_EQUAL(false, unit1->canAttack(unit2));
 		}
 
 		void attackTest()
 		{
-			qrw::Board board(2, 2);
+			player1->setActive(true);
 
+			// Set up terrain
 			qrw::Terrain terrain1(qrw::ET_HILL, -1, +1);
-			qrw::Player  player1;
-			qrw::Unit attacker(qrw::EUT_SPEARMAN, 5, 2, 1, 1, 3, &player1, &board);
-
-			player1.setActive(true);
-			board.getSquare(0, 0)->setTerrain(&terrain1);
-			board.getSquare(0, 0)->setUnit(&attacker);
-			attacker.setSquare(board.getSquare(0, 0));
+			board->getSquare(0, 0)->setTerrain(&terrain1);
 
 			qrw::Terrain terrain2(qrw::ET_HILL, +2, -1);
-			qrw::Player player2;
-			qrw::Unit defender(qrw::EUT_SPEARMAN, 5, 2, 1, 1, 3, &player2, &board);
+			board->getSquare(0, 1)->setTerrain(&terrain2);
 
-			board.getSquare(0, 1)->setTerrain(&terrain2);
-			board.getSquare(0, 1)->setUnit(&defender);
-			defender.setSquare(board.getSquare(0, 1));
-
-			qrw::Unit::AttackResult result = attacker.attack(&defender);
+			qrw::Unit::AttackResult result = unit1->attack(unit2);
 
 			CPPUNIT_ASSERT_EQUAL(true, result.attackPerformed);
-			CPPUNIT_ASSERT_EQUAL(2, result.attackerHPDelta);
+			CPPUNIT_ASSERT_EQUAL(3, result.attackerHPDelta);
 			CPPUNIT_ASSERT_EQUAL(1, result.defenderHPDelta);
 
-			CPPUNIT_ASSERT_EQUAL(3, attacker.getHP());
-			CPPUNIT_ASSERT_EQUAL(4, defender.getHP());
+			CPPUNIT_ASSERT_EQUAL(2, unit1->getHP());
+			CPPUNIT_ASSERT_EQUAL(4, unit2->getHP());
 
 			// Check if attackPerformed is false if attacking was not able.
-			result = defender.attack(&attacker);
+			result = unit2->attack(unit1);
 
 			CPPUNIT_ASSERT_EQUAL(false, result.attackPerformed);
 		}
 
 	private:
+		qrw::Board* board;
+		qrw::Player* player1;
+		qrw::Player* player2;
 		qrw::Unit* unit1;
 		qrw::Unit* unit2;
 };
