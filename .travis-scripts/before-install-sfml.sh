f8c5d012ed070a1da537624656b5bc0ffe03deed
namelessvoid <pommesdiefritte@gmx.de>
2014-08-09 20:56:57 +0200

    Revert "Removed sfml-network from travis build."
    This reverts commit 92e44f4952f499952de3506b8672e20f0b51fd31. Commit intended to remove sfml-network from build, but network component is build by 'make install' anyways.

--- .travis-scripts/before-install-sfml.sh
+++ .travis-scripts/before-install-sfml.sh
@@ -9,9 +9,7 @@ git clone https://github.com/LaurentGomila/SFML.git
 cd SFML
 
 # build and install it
-cmake .
-make sfml-audio sfml-graphics sfml-window sfml-system
-sudo make install
+cmake . && make && sudo make install
 
 # copy FindSFML.cmake
 sudo cp cmake/Modules/FindSFML.cmake /usr/share/cmake-2.8/Modules
