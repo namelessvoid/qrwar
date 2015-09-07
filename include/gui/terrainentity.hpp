69514eacb4f1a42df43a7657d318683a61fecf81
namelessvoid <pommesdiefritte@gmx.de>
2015-09-07 19:17:31 +0200

    Implemented TerrainEntity.

--- include/gui/terrainentity.hpp
+++ include/gui/terrainentity.hpp
@@ -1,13 +1,24 @@
 #ifndef QRW_TERRAINENTITY_H
 #define QRW_TERRAINENTITY_H
 
+#include <SFML/Graphics/RectangleShape.hpp>
+
+#include "engine/terrain.hpp"
+
 namespace qrw
 {
 
-class TerrainEntity
+class TerrainEntity : public sf::RectangleShape
 {
 public:
-	TerrainEntity();
+	typedef std::shared_ptr<TerrainEntity> Ptr;
+
+	static Ptr createTerrainEntity(Terrain::Ptr terrain, int dimension);
+
+private:
+	TerrainEntity(Terrain::Ptr terrain, int dimension);
+
+	Terrain::Ptr _terrain;
 };
 
 } // namespace qrw
--- src/gui/terrainentity.cpp
+++ src/gui/terrainentity.cpp
@@ -1,11 +1,44 @@
 #include "gui/terrainentity.hpp"
 
+#include "gui/texturemanager.hpp"
+#include "engine/coordinates.hpp"
+
 namespace qrw
 {
 
-TerrainEntity::TerrainEntity()
+TerrainEntity::Ptr TerrainEntity::createTerrainEntity(Terrain::Ptr terrain, int dimension)
 {
+	return Ptr(new TerrainEntity(terrain, dimension));
+}
 
+TerrainEntity::TerrainEntity(Terrain::Ptr terrain, int dimension)
+	: _terrain(terrain)
+{
+	setSize(sf::Vector2f(dimension, dimension));
+
+	const Coordinates boardPosition = _terrain->getPosition();
+	setPosition(sf::Vector2f(dimension * boardPosition.getX(), dimension * boardPosition.getY()));
+
+	// Find correct texture
+	const sf::Texture* texture = nullptr;
+
+	switch(_terrain->getType())
+	{
+	case ET_WOOD:
+		texture = TextureManager::getInstance()->getTexture("wood");
+		break;
+	case ET_HILL:
+		texture = TextureManager::getInstance()->getTexture("hill");
+		break;
+	case ET_WALL:
+		texture = TextureManager::getInstance()->getTexture("wall");
+		break;
+	default:
+		texture = TextureManager::getInstance()->getFallbackTexture();
+		break;
+	}
+
+	setTexture(texture);
 }
 
 } // namespace qrw
