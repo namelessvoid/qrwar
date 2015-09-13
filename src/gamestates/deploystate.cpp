79d09555a8677ee14a8e89ae9222200345ffab27
namelessvoid <pommesdiefritte@gmx.de>
2015-09-13 22:05:26 +0200

    Added buttons to DeployState toolbar.

--- src/gamestates/deploystate.cpp
+++ src/gamestates/deploystate.cpp
@@ -4,12 +4,89 @@
 
 #include "gamestates/mapeditorstate.hpp"
 
+#include "gui/texturemanager.hpp"
+
 namespace qrw
 {
 
 DeployState::DeployState(sf::RenderWindow* renderWindow)
 	: SceneState(renderWindow, EGameStateId::EGSID_DEPLOY_STATE)
 {
+	// Initialize tool bar
+	sf::Vector2f buttonSize(140.0f, 50.0f);
+	namelessgui::RadioToggleButton* radioButton = nullptr;
+	namelessgui::Label* label = nullptr;
+	std::shared_ptr<namelessgui::ButtonGroup> unitButtonGroup = nullptr;
+	TextureManager* textureManager = TextureManager::getInstance();
+
+	// Player one tools
+	label = new namelessgui::Label();
+	label->setText("Player One");
+	label->setAnchor({0.5f, 0.0f});
+	label->setParentAnchor({0.5f, 0.0f});
+	_toolBar->addWidget(label);
+
+	radioButton = new namelessgui::RadioToggleButton();
+	unitButtonGroup = radioButton->getButtonGroup();
+	radioButton->setText("Swordman");
+	radioButton->setSize(buttonSize);
+	radioButton->setRelativePosition({5.0f, buttonSize.y});
+	radioButton->setImage(textureManager->getTexture("p1swordman"));
+	_toolBar->addWidget(radioButton);
+
+	radioButton = new namelessgui::RadioToggleButton(unitButtonGroup);
+	radioButton->setText("Archer");
+	radioButton->setSize(buttonSize);
+	radioButton->setRelativePosition({5.0f, 2 * buttonSize.y});
+	radioButton->setImage(textureManager->getTexture("p1archer"));
+	_toolBar->addWidget(radioButton);
+
+	radioButton = new namelessgui::RadioToggleButton(unitButtonGroup);
+	radioButton->setText("Spearman");
+	radioButton->setSize(buttonSize);
+	radioButton->setRelativePosition({5.0f, 3 * buttonSize.y});
+	radioButton->setImage(textureManager->getTexture("p1spearman"));
+	_toolBar->addWidget(radioButton);
+
+	// Player two tools
+	label = new namelessgui::Label();
+	label->setText("Player Two");
+	label->setAnchor({0.5f, 0.0f});
+	label->setParentAnchor({0.5f, 0.4f});
+	_toolBar->addWidget(label);
+
+	radioButton = new namelessgui::RadioToggleButton(unitButtonGroup);
+	radioButton->setText("Swordman");
+	radioButton->setSize(buttonSize);
+	radioButton->setParentAnchor({0.0f, 0.4f});
+	radioButton->setRelativePosition({5.0f, buttonSize.y});
+	radioButton->setImage(textureManager->getTexture("p2swordman"));
+	_toolBar->addWidget(radioButton);
+
+	radioButton = new namelessgui::RadioToggleButton(unitButtonGroup);
+	radioButton->setText("Archer");
+	radioButton->setSize(buttonSize);
+	radioButton->setParentAnchor({0.0f, 0.4f});
+	radioButton->setRelativePosition({5.0f, 2 * buttonSize.y});
+	radioButton->setImage(textureManager->getTexture("p2archer"));
+	_toolBar->addWidget(radioButton);
+
+	radioButton = new namelessgui::RadioToggleButton(unitButtonGroup);
+	radioButton->setText("Spearman");
+	radioButton->setSize(buttonSize);
+	radioButton->setParentAnchor({0.0f, 0.4f});
+	radioButton->setRelativePosition({5.0f, 3 * buttonSize.y});
+	radioButton->setImage(textureManager->getTexture("p2spearman"));
+	_toolBar->addWidget(radioButton);
+
+	// Next step button
+	namelessgui::Button* nextStepButton = new namelessgui::Button();
+	nextStepButton->setText("Into Battle");
+	nextStepButton->setSize({buttonSize.x, 30.0f});
+	nextStepButton->setAnchor({0.5f, 1.0f});
+	nextStepButton->setParentAnchor({0.5f, 1.0f});
+	nextStepButton->setRelativePosition({0.0f, -5.0f});
+	_toolBar->addWidget(nextStepButton);
 }
 
 DeployState::~DeployState()
