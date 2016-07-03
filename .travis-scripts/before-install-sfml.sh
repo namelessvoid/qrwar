f2b10b545a707d1dfc8237eef85f03d83e178eb4
namelessvoid <simon_kerler@web.de>
2016-07-03 23:30:24 +0200

    Added missing libraries.

--- .travis-scripts/before-install-sfml.sh
+++ .travis-scripts/before-install-sfml.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # install dependencies
-sudo apt-get install libpthread-stubs0-dev libgl1-mesa-dev libx11-dev libxrandr-dev libfreetype6-dev libglew1.5-dev libjpeg8-dev libsndfile1-dev libopenal-dev libudev-dev
+sudo apt-get install libpthread-stubs0-dev libgl1-mesa-dev libx11-dev libx11-xcb-dev libxcb-image0-dev libxcb-randr0-dev libxrandr-dev libfreetype6-dev libglew1.5-dev libjpeg8-dev libsndfile1-dev libopenal-dev libudev-dev
 
 # get the latest sfml version
 cd ../..
