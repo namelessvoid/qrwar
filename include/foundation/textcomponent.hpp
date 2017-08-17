c3d948aec3c2abbefbbaa57cd0f4d1f275312980
namelessvoid <simon_kerler@web.de>
2017-08-17 20:28:05 +0200

    Added game component for rendering text

---
+++ include/foundation/textcomponent.hpp
@@ -0,0 +1,34 @@
+#ifndef QRW_TEXTCOMPONENT_HPP
+#define QRW_TEXTCOMPONENT_HPP
+
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+#include "foundation/gamecomponent.hpp"
+#include "rendering/renderable.hpp"
+#include "rendering/rendersystem.hpp"
+
+namespace qrw
+{
+
+class TextComponent : public GameComponent, public Renderable
+{
+public:
+    TextComponent(Layer layer);
+
+    virtual ~TextComponent();
+
+    void setText(const std::string& text);
+
+    virtual void render(sf::RenderTarget &renderTarget) override;
+    virtual void setPosition(const sf::Vector2f &position) override;
+    virtual const sf::Vector2f &getPosition() const override;
+
+private:
+    sf::Font m_font;
+    sf::Text m_text;
+};
+
+} // namespace qrw
+
+#endif // QRW_TEXTCOMPONENT_HPP
---
+++ src/foundation/textcomponent.cpp
@@ -0,0 +1,42 @@
+#include "foundation/textcomponent.hpp"
+
+namespace qrw
+{
+
+qrw::TextComponent::TextComponent(Layer layer)
+	: Renderable(layer)
+{
+	m_font.loadFromFile("./res/font/Knigqst.ttf");
+	m_text.setFont(m_font);
+	m_text.setCharacterSize(20);
+	m_text.setString("DUMMY_TEXT");
+
+	g_renderSystem.registerRenderable(this);
+}
+
+void qrw::TextComponent::setText(const std::string &text)
+{
+	m_text.setString(text);
+}
+
+qrw::TextComponent::~TextComponent()
+{
+	g_renderSystem.deregisterRenderable(this);
+}
+
+void qrw::TextComponent::render(sf::RenderTarget &renderTarget)
+{
+	renderTarget.draw(m_text);
+}
+
+void qrw::TextComponent::setPosition(const sf::Vector2f &position)
+{
+	m_text.setPosition(position);
+}
+
+const sf::Vector2f &qrw::TextComponent::getPosition() const
+{
+	return m_text.getPosition();
+}
+
+} // namespace qrw
