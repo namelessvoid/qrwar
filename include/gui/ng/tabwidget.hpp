d8d45f612ce5bf6091c79a67e9fc36a5edf85999
namelessvoid <pommesdiefritte@gmx.de>
2018-01-06 17:04:09 +0100

    Add TabWidget

---
+++ include/gui/ng/tabwidget.hpp
@@ -0,0 +1,54 @@
+#ifndef NAMELESSGUI_TABWIDGET_HPP
+#define NAMELESSGUI_TABWIDGET_HPP
+
+#include <memory>
+
+#include "widget.hpp"
+
+#include <SFML/Graphics/RectangleShape.hpp>
+
+namespace namelessgui {
+
+class ButtonGroup;
+class RadioToggleButton;
+
+class TabWidget : public Widget
+{
+public:
+    TabWidget();
+
+    ~TabWidget();
+
+    void addTab(const std::string& buttonName, Widget* widget);
+
+    void setButtonSize(const sf::Vector2f& buttonSize);
+
+    void setActiveTab(size_t index);
+
+    // Widget interface
+    virtual sf::FloatRect getGlobalBounds() override;
+    virtual void setSize(const sf::Vector2f& size) override;
+    virtual sf::Vector2f getSize() const override;
+    virtual sf::Vector2f getPosition() const override;
+    virtual void setPosition(const sf::Vector2f& position) override;
+
+private:
+    TabWidget(const TabWidget& rhs) = delete;
+
+    TabWidget& operator=(const TabWidget& rhs) = delete;
+
+    sf::Vector2f getButtonRelativePosition(int index);
+
+    sf::Vector2f size_;
+    sf::Vector2f position_;
+
+    sf::Vector2f buttonSize_;
+
+    std::shared_ptr<ButtonGroup> buttonGroup_;
+    std::vector<RadioToggleButton*> buttons_;
+    std::vector<Widget*> widgets_;
+};
+
+} // namespace namelessgui
+
+#endif // NAMELESSGUI_TABWIDGET_HPP
---
+++ src/gui/ng/tabwidget.cpp
@@ -0,0 +1,104 @@
+#include "gui/ng/tabwidget.hpp"
+
+#include <cassert>
+
+#include "gui/ng/buttongroup.hpp"
+#include "gui/ng/radiotogglebutton.hpp"
+
+namespace namelessgui
+{
+
+TabWidget::TabWidget()
+	: buttonSize_(20, 20)
+{
+	setSize({100, 100});
+	buttonGroup_ = std::make_shared<ButtonGroup>();
+}
+
+TabWidget::~TabWidget()
+{
+}
+
+void TabWidget::addTab(const std::string& buttonName, Widget* widget)
+{
+	assert(widget != nullptr);
+
+	size_t index = buttons_.size();
+
+	RadioToggleButton* button = new RadioToggleButton(buttonGroup_);
+	button->setText(buttonName);
+	button->setSize(buttonSize_);
+	button->setRelativePosition(getButtonRelativePosition(index));
+	button->signalActivated.connect(std::bind(&TabWidget::setActiveTab, this, index));
+	addWidget(button);
+	buttons_.push_back(button);
+
+	widget->setAnchor({0, 0});
+	widget->setParentAnchor({0, 0});
+	widget->setSize({getSize().x, getSize().y - buttonSize_.y});
+	widget->setRelativePosition({0, buttonSize_.y});
+	addWidget(widget);
+	widgets_.push_back(widget);
+	widget->setVisible(false);
+
+	// If this is the first tab, make it active by default
+	if(index == 0)
+	{
+		button->signalclicked.emit();
+		widget->setVisible(true);
+	}
+}
+
+void TabWidget::setButtonSize(const sf::Vector2f& buttonSize)
+{
+	buttonSize_ = buttonSize;
+
+	for(size_t i = 0; i < buttons_.size(); ++i)
+	{
+		Button* button = buttons_.at(i);
+		button->setSize(buttonSize);
+		button->setRelativePosition(getButtonRelativePosition(i));
+	}
+}
+
+void TabWidget::setActiveTab(size_t index)
+{
+	bool tabVisible = false;
+	for(size_t i = 0; i < widgets_.size(); ++i)
+	{
+		tabVisible = i == index;
+		widgets_.at(i)->setVisible(tabVisible);
+	}
+}
+
+sf::FloatRect TabWidget::getGlobalBounds()
+{
+	return sf::FloatRect(getPosition(), getSize());
+}
+
+void TabWidget::setSize(const sf::Vector2f &size)
+{
+	size_ = size;
+}
+
+sf::Vector2f TabWidget::getSize() const
+{
+	return size_;
+}
+
+sf::Vector2f TabWidget::getPosition() const
+{
+	return position_;
+}
+
+void TabWidget::setPosition(const sf::Vector2f &position)
+{
+	position_ = position;
+}
+
+sf::Vector2f TabWidget::getButtonRelativePosition(int index)
+{
+	return { index * buttonSize_.x, 0 };
+}
+
+} // namespace namelessgui
