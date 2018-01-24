fa4d18c8b0d1c0f9d2747a70b7517d30b4c9830e
namelessvoid <pommesdiefritte@gmx.de>
2018-01-24 12:54:46 +0100

    Removed outdated tostring library

--- include/tostring.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TOSTRING_HPP
-#define TOSTRING_HPP
-#include <string>
-
-std::string intToString(int number);
-std::string floatToString(float number);
-std::string doubleToString(double number);
-
-#endif
