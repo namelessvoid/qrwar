7c0fc719c174f67aa82c8d00e320e084cfd19797
namelessvoid <pommesdiefritte@gmx.de>
2018-04-20 23:26:20 +0200

    Render deployment zone on top of terrain

--- include/game/renderlayers.hpp
+++ include/game/renderlayers.hpp
@@ -4,8 +4,8 @@
 #include "rendering/rendersystem.hpp"
 
 #define RENDER_LAYER_BACKGROUND Layer(0)
-#define RENDER_LAYER_DEPLOYMENT_ZONE Layer(8)
 #define RENDER_LAYER_TERRAIN    Layer(10)
+#define RENDER_LAYER_DEPLOYMENT_ZONE Layer(11)
 #define RENDER_LAYER_UNIT       Layer(20)
 #define RENDER_LAYER_PATH       Layer(21)
 #define RENDER_LAYER_CURSOR     Layer(22)
