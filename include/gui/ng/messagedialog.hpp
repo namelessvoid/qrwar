9f37a2731ecc449c8700bf38959440277563942f
namelessvoid <pommesdiefritte@gmx.de>
2018-05-13 19:47:42 +0200

    Add MessageDialog to display messages to the user

---
+++ include/gui/ng/messagedialog.hpp
@@ -0,0 +1,36 @@
+#ifndef NAMELESSGUI_MESSAGEDIALOG_HPP
+#define NAMELESSGUI_MESSAGEDIALOG_HPP
+
+#include "gui/ng/window.hpp"
+#include "gui/ng/label.hpp"
+#include "gui/ng/button.hpp"
+
+namespace namelessgui {
+
+class MessageDialog : public Window
+{
+public:
+	MessageDialog();
+
+	~MessageDialog();
+
+	virtual void setSize(const sf::Vector2f& size) override;
+
+	inline void setMessage(const std::string& message) { label_->setText(message); }
+	inline void setButtonText(const std::string& text) { closeButton_->setText(text); }
+
+	Signal<> signalClosed;
+
+private:
+	MessageDialog(const MessageDialog& rhs) = delete;
+
+	MessageDialog& operator=(const MessageDialog& rhs) = delete;
+
+	Label* label_;
+	Button* closeButton_;
+};
+
+
+} // namespace namelessgui
+
+#endif // NAMELESSGUI_MESSAGEDIALOG_HPP
---
+++ src/gui/ng/messagedialog.cpp
@@ -0,0 +1,34 @@
+#include "gui/ng/messagedialog.hpp"
+
+namespace namelessgui
+{
+
+MessageDialog::MessageDialog()
+{
+    label_ = new Label();
+    label_->setText("Dummy text");
+    label_->setWordWrap(true);
+    addWidget(label_);
+
+    closeButton_ = new Button();
+    closeButton_->setText("Close");
+    closeButton_->signalClicked.connect([this] { signalClosed.emit(); });
+    closeButton_->setAnchor({0.5, 1.0});
+    closeButton_->setParentAnchor({0.5, 1.0});
+    closeButton_->setRelativePosition({0.0, -5.0});
+    addWidget(closeButton_);
+
+    setSize({300.0, 200.0});
+}
+
+MessageDialog::~MessageDialog()
+{
+}
+
+void MessageDialog::setSize(const sf::Vector2f& size)
+{
+    Window::setSize(size);
+    label_->setSize(size);
+}
+
+} // namespace namelessgui
